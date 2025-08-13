34b43d0d7c726cc76ca57a6e4c703631aee6c135
Ayush jha <ayu.jha321@gmail.com>
2025-08-14 01:21:17 +0530

    changed axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const API_BASE_URL =
   
   import.meta.env.MODE === "production"
-    ?"https://your-render-api.onrender.com/api"  // local backend
+    ?"https://bugverse-app.onrender.com/api"  // local backend
     : "http://localhost:5000/api" ; // render production backend
 
 
